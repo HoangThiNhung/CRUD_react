7282b695f9773f68010ce8817799879b8a7b9850
oHoangThiNhung <hoang.thi.nhung@framgia.com>
2017-09-19 15:27:56 +0700

    react delete an item

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -13,16 +13,34 @@ var Body = React.createClass({
     });
   },
 
-  handleSubmit(item) {
+  handleSubmit: function(item) {
     var newState = this.state.items.concat(item);
     this.setState({ items: newState });
+  },
+
+  handleDelete: function(id) {
+    $.ajax({
+      url: '/api/v1/items/' + id,
+      type: 'DELETE',
+      success:() => {
+        this.removeItemClient(id);
+      }
+    });
+  },
+
+  removeItemClient: function(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+
+    this.setState({ items: newItems });
   },
 
   render() {
     return (
       <div>
         <NewItem handleSubmit={this.handleSubmit} />
-        <AllItems items={this.state.items} />
+        <AllItems items={this.state.items} handleDelete={this.handleDelete} />
       </div>
     )
   }
